21ff3363f5a8871aabbb75a9795e7c980b2a4435
sa2taka <sa2taka@gmail.com>
2017-10-31 23:47:45 +0900

    追加: ページネーションを追加

--- src/ajaxMix.js
+++ src/ajaxMix.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-const URL_BASE = 'http://infinite-river-99979.herokuapp.com/api/'
+const URL_BASE = 'http://localhost/api/'
 
 export default {
   methods: {
-    get_ajax (url, name) {
+    get_ajax (url, name, klass, callback) {
       return axios.get(URL_BASE + url)
       .then((res) => {
         Vue.set(this, name, res.data)
+        callback(klass)
       })
     }
   }
--- src/components/Names.vue
+++ src/components/Names.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="table-responsive">
+  <div>
     <form>
       <p>input: <input type="text" v-model="searchWord" @keyup="update_names()"></p>
       <b-form-radio-group v-model="selected"
@@ -9,7 +9,24 @@
       </b-form-radio-group>
     </form>
 
-    <b-table striped hover :items="names" :fields="fields"></b-table>
+    <!-- paginationの設定 -->
+    <div class="row my-1">
+      <div class="col-sm-8">
+        <b-pagination :total-rows="totalRows" :per-page="perPage" v-model="currentPage" />
+      </div>
+      <div class="col-md-4 text-md-right">
+        <b-form-fieldset horizontal label="1ページあたりの表示数" :label-cols="6">
+          <b-form-select :options="pageOptions" v-model="perPage" />
+        </b-form-fieldset>
+      </div>
+    </div>
+
+    <b-table striped hover show-empty
+             :items="names"
+             :fields="fields"
+             :current-page="currentPage"
+             :per-page="perPage">
+    </b-table>
   </div>
 </template>
 
@@ -54,21 +71,29 @@ export default {
       searchOptions: [
         { text: 'usernameで検索', value: 'username' },
         { text: 'idで検索', value: 'id' }
-      ]
+      ],
+      currentPage: 1,
+      perPage: 5,
+      totalRows: 0,
+      pageOptions: [ { text: 5, value: 5 }, { text: 10, value: 10 }, { text: 15, value: 15 } ],
+      modalDetails: { index: '', data: '' }
     }
   },
   methods: {
     update_names () {
-      console.log('update..')
       let options = ''
       if (this.searchWord !== '') {
         options = '?' + this.selected + '=' + this.searchWord
       }
-      this.get_ajax('names' + options, 'names')
+      this.get_ajax('names' + options, 'names', this, function (klass) {
+        klass.totalRows = klass.names.length
+      })
     }
   },
   created () {
-    this.get_ajax('names', 'names')
+    this.get_ajax('names', 'names', this, function (klass) {
+      klass.totalRows = klass.names.length
+    })
   }
 }
 </script>
